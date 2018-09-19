d5891312feeaaaacdcc841c2879d86a1131db20b
Tobias Bleckert <tobias.bleckert@iis.se>
2018-09-19 14:53:29 +0200

    Run lints after webpack build

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,25 +1,35 @@
 const mix = require('laravel-mix');
+const path = require('path');
+const WebpackShellPlugin = require('webpack-shell-plugin');
 
 mix.webpackConfig({
+	plugins: [
+		new WebpackShellPlugin({
+			onBuildEnd: ['yarn run eslint', 'yarn run sasslint', 'yarn run phplint'],
+			dev: false,
+		}),
+	],
 	resolve: {
 		modules: [
-			path.resolve(__dirname, 'node_modules')
-		]
+			path.resolve(__dirname, 'node_modules'),
+		],
 	},
 	module: {
-		rules: [{
-			test: /\.js?$/,
-			use: [{
-				loader: 'babel-loader',
-				options: mix.config.babel()
-			}]
-		}]
-	}
+		rules: [
+			{
+				test: /\.js?$/,
+				use: [{
+					loader: 'babel-loader',
+					options: mix.config.babel(),
+				}],
+			},
+		],
+	},
 });
 
 mix
 	.js('./resources/js/site.js', './assets/js/site.js')
 	.sass('./resources/scss/site.scss', './assets/css')
-	.browserSync('iis-start.test')
+	//.browserSync('iis-start.test')
 	.version()
 	.setPublicPath('./');
