141a97d60f479168fe2cc24b23c6c88521fe46bf
Tobias Bleckert <tobias.bleckert@iis.se>
2018-10-03 11:02:53 +0200

    Added a very basic grid

--- www/app/themes/iis-start/index.php
+++ www/app/themes/iis-start/index.php
@@ -1,7 +1,18 @@
 <?php get_header(); ?>
 <div class="iis-start-o-main">
-	The page layout is defined in a structure called site. Each parts are then organisms that at some point will contain molecules and atoms.
-	Start a new theme by defining the page layout and take it from there.
+	<p>The page layout is defined in a structure called site. Each parts are then organisms that at some point will contain molecules and atoms.
+		Start a new theme by defining the page layout and take it from there.</p>
+
+	<p>There's also a structure called grid that you can use with our without gutters. Configure the gutter width in the layout configuration</p>
+
+	<div class="grid grid-cols-6 grid-gap-1">
+		<div class="iis-start-a-card">Col 1</div>
+		<div class="iis-start-a-card">Col 2</div>
+		<div class="iis-start-a-card">Col 3</div>
+		<div class="iis-start-a-card">Col 4</div>
+		<div class="iis-start-a-card">Col 5</div>
+		<div class="iis-start-a-card">Col 6</div>
+	</div>
 </div>
 <aside class="iis-start-o-sidebar">
 	Sidebar
