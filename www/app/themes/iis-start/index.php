248ea131c24963c3a39b6f43ac33382f63371310
Tobias Bleckert <hola@tobiasbleckert.se>
2020-05-19 13:43:31 +0200

    Set block classes

--- www/app/themes/iis-start/index.php
+++ www/app/themes/iis-start/index.php
@@ -2,9 +2,9 @@
 <div class="iis-start-site__main__content">
 	<div class="wrapper">
 		<div class="row justify-content-center">
-			<div class="grid-18">
+			<div class="grid-18 wp-blocks-wrapper">
 				<div class="iis-start-section">
-					<div class="iis-start-article">
+					<div class="iis-start-article wp-blocks-content">
 						<h1><?php the_title(); ?></h1>
 						<?php the_content(); ?>
 					</div>
