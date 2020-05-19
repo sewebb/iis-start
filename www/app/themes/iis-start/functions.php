985e871a5b1da9bfb6238fe133023f1fca68c579
Tobias Bleckert <hola@tobiasbleckert.se>
2020-05-19 15:01:12 +0200

    New block setup

--- www/app/themes/iis-start/functions.php
+++ www/app/themes/iis-start/functions.php
@@ -11,6 +11,49 @@ if ( ! function_exists( 'iis_start_setup' ) ) {
 		add_theme_support( 'automatic-feed-links' );
 		add_theme_support( 'title-tag' );
 		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'align-wide' );
+
+		add_theme_support( 'disable-custom-colors' );
+		add_theme_support(
+			'editor-color-palette',
+			[
+				[
+					'name'  => 'Ocean dark',
+					'slug'  => 'ocean-dark',
+					'color' => '#0477ce',
+				],
+				[
+					'name'  => 'Ruby light',
+					'slug'  => 'ruby-light',
+					'color' => '#ff9fb4',
+				],
+				[
+					'name'  => 'Ruby dark',
+					'slug'  => 'ruby-dark',
+					'color' => '#d9002f',
+				],
+				[
+					'name'  => 'Jade light',
+					'slug'  => 'jade-light',
+					'color' => '#aae3d9',
+				],
+				[
+					'name'  => 'Jade dark',
+					'slug'  => 'jade-dark',
+					'color' => '#2d897a',
+				],
+				[
+					'name'  => 'Lemon',
+					'slug'  => 'lemon',
+					'color' => '#ffce2e',
+				],
+				[
+					'name'  => 'Lemon light',
+					'slug'  => 'lemon-light',
+					'color' => '#ffe696',
+				],
+			]
+		);
 	}
 }
 
--- www/app/themes/iis-start/index.php
+++ www/app/themes/iis-start/index.php
@@ -5,13 +5,14 @@ the_post();
 
 ?>
 <div class="iis-start-site__main__content">
-	<div class="wrapper">
-		<div class="row justify-content-center">
-			<div class="grid-18 wp-blocks-wrapper">
-				<div class="iis-start-section">
-					<div class="iis-start-article wp-blocks-content">
-						<h1><?php the_title(); ?></h1>
-						<?php the_content(); ?>
+	<div class="iis-start-section<?php echo ( iis_has_hero() ) ? ' u-m-t-0' : ''; ?>">
+		<div class="wrapper">
+			<div class="row justify-content-center">
+				<div class="grid-18">
+					<div class="iis-start-article">
+						<div class="iis-start-article__content">
+							<?php the_content(); ?>
+						</div>
 					</div>
 				</div>
 			</div>
