9ff880fcfb0cf3ac2391d66a328fe4fc4f4b57ef
Tobias Bleckert <hola@tobiasbleckert.se>
2020-05-20 14:59:34 +0200

    Use iis-library setup

--- www/app/themes/iis-start/functions.php
+++ www/app/themes/iis-start/functions.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * IIS Start functions and definitions
+ *
+ * @package iis-start
+ */
+
+use Internetstiftelsen\Theme;
+
+Theme::init();
 
 if ( ! function_exists( 'iis_start_setup' ) ) {
 	/**
@@ -7,53 +16,6 @@ if ( ! function_exists( 'iis_start_setup' ) ) {
 	 */
 	function iis_start_setup() {
 		load_theme_textdomain( 'iis-start', get_template_directory() . '/resources/languages' );
-
-		add_theme_support( 'automatic-feed-links' );
-		add_theme_support( 'title-tag' );
-		add_theme_support( 'post-thumbnails' );
-		add_theme_support( 'align-wide' );
-
-		add_theme_support( 'disable-custom-colors' );
-		add_theme_support(
-			'editor-color-palette',
-			[
-				[
-					'name'  => 'Ocean dark',
-					'slug'  => 'ocean-dark',
-					'color' => '#0477ce',
-				],
-				[
-					'name'  => 'Ruby light',
-					'slug'  => 'ruby-light',
-					'color' => '#ff9fb4',
-				],
-				[
-					'name'  => 'Ruby dark',
-					'slug'  => 'ruby-dark',
-					'color' => '#d9002f',
-				],
-				[
-					'name'  => 'Jade light',
-					'slug'  => 'jade-light',
-					'color' => '#aae3d9',
-				],
-				[
-					'name'  => 'Jade dark',
-					'slug'  => 'jade-dark',
-					'color' => '#2d897a',
-				],
-				[
-					'name'  => 'Lemon',
-					'slug'  => 'lemon',
-					'color' => '#ffce2e',
-				],
-				[
-					'name'  => 'Lemon light',
-					'slug'  => 'lemon-light',
-					'color' => '#ffe696',
-				],
-			]
-		);
 	}
 }
 
